156735cb2e36187f83253e119c599feaac6571dd
marusiakot <kotova_marusia@mail.ru>
2017-10-19 12:55:57 +0000

    migrate files

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ActiveRecord::Base
-    
+    validates :title, presence: true, length: {minimum: 8, maximum: 56}
+    validates :description, presence: true, length: {minimum: 150, maximum: 580}
 end
