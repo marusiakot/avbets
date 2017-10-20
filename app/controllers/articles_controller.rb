49ddf7056a2c48befab30d48b5b094f3a196e075
marusiakot <kotova_marusia@mail.ru>
2017-10-20 10:27:27 +0000

    base till articles creating

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,18 @@
 class ArticlesController < ApplicationController
+    
+    before_action :set_article, only: [:edit, :destroy, :update, :show]
+
+   
+    def index
+       @articles = Article.all
+    end
+   
     def new
        @article = Article.new 
+    end
+    
+    def edit
+     
     end
     
     def create
@@ -8,17 +20,40 @@ class ArticlesController < ApplicationController
       
       if @article.save
       flash[:notice] = "Ваш пост был удачно опубликован!"
-          reditect_to articles_path(@article)
+          redirect_to articles_path(@article)
       else
           render :new 
       end
     end
     
+def update
+   
+        if @article.update(article_params)
+            flash[:notice] = "Article was successfully updated"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
+end
+    
     def show
-        @article = Article.find(params[:id])
+       
+    end
+    
+    def destroy
+
+        @article.destroy
+        
+        flash[:notice] = "Пост был успешно удален!"
+        redirect_to articles_path
     end
     
     private
+    
+    def set_article
+         @article = Article.find(params[:id])
+    end
+    
     def article_params
        params.require(:article).permit(:title, :description) 
     end
